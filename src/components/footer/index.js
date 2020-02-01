fab5ea9cb9e4b30a1e8d664618280700f5292a16
Rafael De Leon <rafael_de_leon88@hotmail.com>
2020-02-01 13:23:41 -0500

    added components

---
+++ src/components/footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function footer() {
+  return (
+    <div>
+      <div className='row'>
+        <div className='col-md-4'>
+          <h1>test</h1>
+        </div>
+        <div className='col-md-4'>
+          <h1>test</h1>
+        </div>
+        <div className='col-md-4'>
+          <h1>test</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
